fc9ce755a1999c443ad97b7faf240190a307bae2
Konstantin Lugovskikh <kostya.lugovskikh@mail.ru>
2024-01-26 16:01:38 +0500

    Список CRUD

--- hexlet_django_blog/article/admin.py
+++ hexlet_django_blog/article/admin.py
@@ -1,3 +1,11 @@
+from django.contrib.admin import DateFieldListFilter
 from django.contrib import admin
+from .models import Article
 
-# Register your models here.
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('name', 'timestamp')
+    search_fields = ['name', 'body']
+    list_filter = (('timestamp', DateFieldListFilter),)
+
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,8 +1,13 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 from django.views import View
+
+from hexlet_django_blog.article.models import Article
 
 
 class IndexView(View):
 
     def get(self, request, *args, **kwargs):
-        return HTTPResponse('Hello, World!')
+        articles = Article.objects.all()[:15]
+        return render(request, 'articles/index.html', context={
+            'articles': articles,
+        })
